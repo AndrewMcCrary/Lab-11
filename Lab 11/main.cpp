a8fc1f8c1ad9837a3956da2c6ce331017fa23a54
Richard Roberts <richard.robertsf@gmail.com>
2022-04-18 22:08:15 -0400

    added another option to main

--- Lab 11/main.cpp
+++ Lab 11/main.cpp
@@ -9,54 +9,44 @@ int main(void) {
 	Graphy graph;
 
 	graph.addVertex(1);
-	graph.addVertex(2);
 	graph.addVertex(3);
-	graph.addVertex(4);
 	graph.addVertex(5);
-	graph.addVertex(6);
+	graph.addVertex(7);
+	graph.addVertex(9);
+	graph.addVertex(11);
 
 	graph.addEdge(1, 5);
-	graph.addEdge(1, 2);
+	graph.addEdge(1, 7);
 	graph.addEdge(1, 3);
 	graph.addEdge(5, 3);
-	graph.addEdge(3, 4);
-	graph.addEdge(3, 2);
-	graph.addEdge(4, 2);
+	graph.addEdge(3, 9);
+	graph.addEdge(3, 1);
+	graph.addEdge(11, 7);
 
 	bool connectionTerminated = false;
 	while (!connectionTerminated) {
-		std::cout << "Graph Function Testing Menu\n\n [1] Add Edge\n [2] Remove Edge\n [3] Has Edge\n [4] Out Edges\n" <<
-			" [5] In Edges\n [6] DFS\n [7] BFS\n";
+		std::cout << "\nGraph Function Testing Menu\n\n [1] Add Edge\n [2] Remove Edge\n [3] Has Edge\n [4] Has Vertex\n [5] Out Edges\n" <<
+			" [6] In Edges\n [7] DFS\n [8] BFS\n";
 		int choice = 0;
 		std::cin >> choice;
 		bool correct = true;
 		int base, dest;
 		switch (choice) {
 		case 1:
-			while (correct) {
+			std::cout << "\nYou selected [1] Add Edge. The following data will need to be provided:\n" <<
+				"Base Number | Destination Number\n\n";
+			std::cout << "Please provide the following data.\nEnter a base: ";
+			std::cin >> base;
+			std::cout << "Enter a destination: ";
+			std::cin >> dest;
 
-				std::cout << "\nYou selected [1] Add Edge. The following data will need to be provided:\n" <<
-					"Base Number | Destination Number\n\n";
-				std::cout << "Please provide the following data.\nEnter a base: ";
-				std::cin >> base;
-				std::cout << "Enter a destination: ";
-				std::cin >> dest;
-				std::cout << "\nBase: " << base << "\nDestination: " << dest << "\n";
-				std::cout << "\nIs this correct? [0]: yes / [1]: no | ";
-				std::cin >> correct;
-
-				if (!correct) {
-					if (graph.hasEdge(base, dest))
-						std::cout << "Edge already exists. \n";
-					else if (!(graph.hasVertex(base)))
-						std::cout << "The base you have entered does not exist. \n";
-					else if (!(graph.hasVertex(dest)))
-						std::cout << "The destination you have entered does not exist. \n";
-					else {
-						graph.addEdge(base, dest);
-						std::cout << "Edge has been added. \n";
-					}
-				}
+			if (graph.hasEdge(base, dest))
+				std::cout << "Edge already exists. \n";
+			else if (!(graph.hasVertex(dest)) || !graph.hasVertex(base))
+				std::cout << "The base or destination you have entered does not exist. \n";
+			else {
+				graph.addEdge(base, dest);
+				std::cout << "Edge has been added. \n";
 			}
 			break;
 		case 2:
@@ -81,19 +71,26 @@ int main(void) {
 			}
 			break;
 		case 3:
-			std::cout << "\nYou selected [3] Has Edge.Please provide the following data.\nEnter a base: ";
+			std::cout << "\nYou selected [3] Has Edge. Please provide the following data.\nEnter a base: ";
 			std::cin >> base;
 			std::cout << "Enter a destination: ";
 			std::cin >> dest;
 
-			if (graph.hasEdge(base, dest)) {
+			if (graph.hasEdge(base, dest))
 				std::cout << "This Edge DOES Exists." << endl;
-			}
-			else {
+			else
 				std::cout << "This Edge DOES NOT Exist " << endl;
-			}
 			break;
 		case 4:
+			std::cout << "\nYou selected [4] Has Vertex. Please provide the following data.\nEnter a vertex: ";
+			std::cin >> base;
+
+			if (graph.hasVertex(base))
+				std::cout << "This vertex DOES Exists." << endl;
+			else
+				std::cout << "This vertex DOES NOT Exist " << endl;
+			break;
+		case 5:
 			std::cout << "\nYou selected [4] Out Edges. Please provide the following data.\nEnter a base: ";
 			std::cin >> base;
 			
@@ -107,7 +104,7 @@ int main(void) {
 				} 
 			}
 			break;
-		case 5:
+		case 6:
 			std::cout << "\nYou selected [5] In Edges. Please provide the following data.\nEnter a destination: ";
 			std::cin >> dest;
 			if (graph.inEdges(dest).empty())
@@ -120,7 +117,7 @@ int main(void) {
 				} 
 			}
 			break;
-		case 6:
+		case 7:
 			std::cout << "\nYou selected [6] DFS. Please provide the following data.\nEnter a base: ";
 			std::cin >> base;
 			std::cout << "Enter a destination: ";
@@ -139,7 +136,7 @@ int main(void) {
 			else
 				std::cout << "There is no path.";
 			break;
-		case 7:
+		case 8:
 			std::cout << "\nYou selected [7] BFS. Please provide the following data.\nEnter a base: ";
 			std::cin >> base;
 			std::cout << "Enter a destination: ";
